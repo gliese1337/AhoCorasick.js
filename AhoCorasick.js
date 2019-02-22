a01f1796af87f8b683d6c667587819dac58c2157
Logan Kearsley <chronosurfer@gmail.com>
2019-02-22 12:57:18 -0700

    Upgrade to modern syntax.

--- AhoCorasick.js
+++ AhoCorasick.js
@@ -1,98 +1,77 @@
-var AhoCorasick = (function(){
+const AhoCorasick = (function() {
 	"use strict";
 
-	function ACNode(out, i){
+	function ACNode (out, i) {
 		this.go = {};
 		this.fail = null;
 		this.out = out;
 		this.index = i;
 	}
 
-	function ACIterator(q,s){
-		var v, i = 0,
-			e = s.length - 1,
-			root = q;
-
-		function mapout(o){
-			var len = o.len,
-				start = i-len+1;
+	ACNode.prototype.search = function * (s, skip = false) {
+		let i = 0;
+		const mapout = ({ length, value }) => {
+			const start = i-length+1;
 
 			return {
-				start: start,
-				len: len,
-				key: s.substr(start,len),
-				value: o.value
+				start, length, value,
+				key: s.substr(start,length),
 			};
 		}
 
-		this.next = function(){
-			if(i > e){
-				return {value: void 0, done: true};
-			}
+		const q = this;
+		const e = s.length;
 
-			var c = s[i];
-
-			while(!q.go.hasOwnProperty(c) && q !== root){
-				q = q.fail;
-			}
-
-			q = q.go[c] || root;
-			v = q.out.map(mapout);
-
-			return {
-				value: v,
-				done: (++i) > e
-			};
-		};
-	}
-
-	ACNode.prototype.search = function(s){
-		return new ACIterator(this,s);
-	};
-
-	ACNode.prototype.findall = function(s){
-		var i, c, q = this,
-			e = s.length,
-			out = [];
-
-		function mapout(o){
-			var len = o.len,
-				start = i-len+1;
-
-			return {
-				start: start,
-				len: len,
-				key: s.substr(start,len),
-				value: o.value
-			};
-		}
-
-		for(i = 0; i < e; i++){
-			c = s[i];
+		for (; i < e; i++){
+			const c = s[i];
 			while(!q.go.hasOwnProperty(c) && q !== this){
 				q = q.fail;
 			}
 			q = q.go[c] || this;
-			[].push.apply(out,q.out.map(mapout));
-		}
-		return out;
+			if (!(skip && q.out.length === 0)) {
+				yield { index: i, matches: q.out.map(mapout) };
+			}
+		}		
 	};
 
-	ACNode.prototype.toJSON = function(){
-		var r, ngo,
-			queue = [this],
-			output = [];
-
-		function updateQueue(a){
-			ngo[a] = r.go[a].index;
-			queue.push(r.go[a]);
+	ACNode.prototype.test = function(s) {
+		const { value, done } = this.search(s, true).next();
+		return !done || !!value;
+	};
+	
+	ACNode.prototype.findall = function(s) {
+		return [...this.search(s, true)];
+	};
+	
+	ACNode.prototype.findallstart = function(s) {
+		const startgroups = new Map();
+		for (const { matches } of this.search(s, true)) {
+			for (const match of matches) {
+				if (startgroups.has(match.start)){
+					startgroups.get(match.start).push(match);
+				} else {
+					startgroups.set(match.start, [match]);
+				}
+			}
 		}
+		
+		return [...startgroups.entries()]
+			.sort((a, b) => a[0] - b[0])
+			.map(([ , v ]) => v);
+	};
 
-		while(queue.length > 0){
-			r = queue.shift();
-			ngo = {};
+	ACNode.prototype.toJSON = function() { 
+		const queue = [this];
+		const output = [];
 
-			Object.keys(r.go).forEach(updateQueue);
+		while (queue.length > 0) {
+			const r = queue.shift();
+			const ngo = {};
+
+			for (const k of Object.keys(r.go)) {
+				ngo[k] = r.go[k].index;
+				queue.push(r.go[k]);
+			}
 
 			output.push({
 				go: ngo,
@@ -102,93 +81,82 @@ var AhoCorasick = (function(){
 			});
 		}
 
-		return {
-			nodes: output.sort(function(a,b){
-				return a.index < b.index?-1:1;
-			})
-		};
+		output.sort((a,b) => a.index - b.index);
+		
+		for (const n of output) {
+			delete n.index;
+		}
+		
+		return { nodes: output };
 	};
 
-	function BuildACTable(keys){
-		var q, queue, r,
-			idx = 0,
-			root = new ACNode([],0);
-
+	function buildTable(keys) {
+		const root = new ACNode([],0);
 		root.fail = root;
 
 		//Phase 1: build a trie
-		keys.forEach(function(key){
-			var i, c, e, str, val;
+		let idx = 0;
+		for (const key of keys) {
+			const { key: str, value } = (typeof key === "object") ? key : { key };
 
-			if(typeof key === "object"){
-				str = key.key;
-				val = key.value;
-			}else{
-				str = key+"";
-			}
-
-			q = root;
-			e = str.length;
-
-			for(i = 0; i < e; i++){
-				c = str[i];
+			let q = root;
+			for (const c of str) {
 				if(!q.go.hasOwnProperty(c)){
-					q.go[c] = new ACNode([],++idx);
+					q.go[c] = new ACNode([],idx++);
 				}
 				q = q.go[c];
 			}
 
-			q.out.push({len: e, value: val});
+			q.out.push({len: str.length, value});
 		});
 
 		//Phase 2: add failure links
 
 		// Set failure links for depth-1 states to root
-		queue = Object.keys(root.go).map(function(c){ return root.go[c]; });
-		queue.forEach(function(r){ r.fail = root; });
+		const queue = Object.keys(root.go).map((c) => root.go[c]);
+		queue.forEach((r) => { r.fail = root; });
 
 		// Compute states of depth d+1 from states of depth d
-
-		function updateQueue(a){
-			var state, s = r.go[a];
-			queue.push(s);
-			state = r.fail;
-			while(!state.go.hasOwnProperty(a) && state !== root){
-				state = state.fail;
-			}
-			s.fail = state.go[a] || root;
-			[].push.apply(s.out, s.fail.out);
-		}
-
-		while(queue.length > 0){
+		while (queue.length > 0) {
 			r = queue.shift();
-			Object.keys(r.go).forEach(updateQueue);
+			for (const k of Object.keys(r.go)) {
+				const s = r.go[k];
+				queue.push(s);
+			
+				let state = r.fail;
+				while(!state.go.hasOwnProperty(k) && state !== root){
+					state = state.fail;
+				}
+
+				s.fail = state.go[k] || root;
+				s.out.push(...s.fail.out);
+			}
 		}
 
 		return root;
 	}
 
-	function ReadACTable(json){
+	function readTable(json) {
 
 		//Transform generic JSON objects into ACNodes
-		var nodes = json.nodes.map(function(n,i){
-			return new ACNode(n.out,i);
-		});
+		const nodes = json.nodes.map((n,i) => [ n, new ACNode(n.out, i) ]);
 
 		//Hook them all up properly
-		json.nodes.forEach(function(n,i){
-			var acn = nodes[i];
-			acn.fail = nodes[n.fail];
-			Object.keys(n).forEach(function(k){
-				acn.go[k] = nodes[n.go[k]];
-			});
-		});
+		for (const [ jn, acn ] of nodes) {
+			acn.fail = nodes[jn.fail];
+			for (const k of Object.keys(jn.go)) {
+				acn.go[k] = nodes[jn.go[k]];
+			}
+		}
 
-		return nodes[0];
+		return nodes[0][1];
 	}
 
 	return {
-		readTable: ReadACTable,
-		buildTable: BuildACTable
+		readTable, buildTable
 	};
 }());
+
+if(module) {
+	module.exports = AhoCorasick;
+}
--- README.md
+++ README.md
@@ -3,25 +3,30 @@ Aho-Corasick.js
 
 This is a JavaScript implementation of the Aho-Corasick String Matching algorithm invented by Alfred V. Aho and Margaret J. Corasick and described in [Efficient String Matching: An Aid to Bibliographic Search](http://cr.yp.to/bib/1975/aho.pdf).
 
-Aho-Corasick is designed to simultaneously match multiple search strings (or "needles") in a single pass over the target text (or "haystack"). This is done in two passes: First, an automaton is constructed which can be re-used to efficiently search for the same set of needles in multiple haystacks. Second, the automaton is run on a specific haystack.
+Aho-Corasick is designed to simultaneously match multiple search strings (or "needles") in a single pass over the target text (or "haystack"). This is done in two stages: First, the needles are pre-processed to construct an automaton which can be re-used to efficiently search for the same set of needles in multiple haystacks. Second, the automaton is run on a specific haystack.
 
-This implementation allows for finding all matches in a given haystack at once, or iterating over each position in the haystack one at a time and returning the matches that end at the current position. Unlike some other implementations, in either case *all* matches are returned- not just the longest or earliest ones.
-
-Methods are also provided for serializing an automaton to JSON and loading from JSON, so that reconstructing an automaton from the original set of needles can be avoided if the same set is going to be used over and over again in different runs of a program.
+This implementation allows for finding all matches in a given haystack at once, or iterating over each position in the haystack one at a time and returning the matches that end at the current position. Unlike some other implementations, in either case *all* matches are returned--not just the longest or earliest ones.
 
 This implementation also allows pairing needles with auxiliary values that are retrieved whenever a needle is matched. This provides a slight memory and time efficiency improvement over matching a string and then doing a map lookup afterwards. This is particularly useful for, e.g., natural language processing applications, where you may want to associate grammatical or lexicographical information with matched strings.
 
 API
 ----
 
-* `AhoCorasick.readTable(json)` Loads a previosuly serialized automaton / lookup table object from serialized JSON.
 * `AhoCorasick.buildTable(needles)` Constructs a new automaton / lookup table from a list of strings to search for. This can be either a simple array of strings, or an array of `{key: ..., value: ...}` objects, where the `key`s are the search needles and the `value`s are arbitrary objects that will be returned when the associated needle is matched.
 
-Both of these static methods return ACNode objects.
+* `AhoCorasick.readTable(json)` Loads a previosuly serialized automaton / lookup table object from serialized JSON.
 
-* `ACNode.search(haystack)` Given a haystack string to search, returns an iterator (object with a `.next()` method) which produces the list of matches at each position in the haystack string.
+Both of these methods return ACNode objects.
+
+* `ACNode.search(haystack, skip = false)` Given a haystack string to search, returns an iterator which produces the list of matches at each position in the haystack string. By default, this will yield a value for every position in the string, with an empty list at positions where no matches terminate. To iterate over only matching positions, pass in `true` for the `skip` parameter.
+
+* `ACNode.test(haystack)` Efficiently determines whether a haystack string contains any matches, without finding all matches.
+
 * `ACNode.findall(haystack)` Returns a single list of all matches in a given haystack string at once.
 
-Both oof these methods return lists of result objects of the form `{start: Int, len: Int, key: String, value: Object}`, where `key` is the needles that was matched and `value` is the associated data, if any.
+* `ACNode.findallstart(haystack)` Returns a list of all matches in a given haystack, regrouped according to their stating rather than ending positions.
 
 * `ACNode.toJSON()` Returns a flattened version of the search automaton which contains no circular references for use by `JSON.serialize()`.
+
+The `search`, `findall`, and `findallstart` methods all return results in objects of the form `{start: Int, length: Int, key: String, value: Object}`, where `key` is the needle that was matched and `value` is the associated data, if any.
+
